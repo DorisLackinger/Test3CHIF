3098a9e2158835fa1b432ec8576f3ed9f2fe2983
Tschernko <tschernko@spengergasse.at>
2016-12-14 20:30:47 +0100

    Mit mocking

---
+++ Test3CHIF/IDate.cs
@@ -0,0 +1,8 @@
+﻿namespace TestUeben {
+    public interface IDate {
+        int Day { get; set; }
+        int DayOfYear { get; }
+        int Month { get; set; }
+        int Year { get; set; }
+    }
+}
---
+++ UnitTestProject1/DateTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestUeben;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestUeben.Tests {
+    [TestClass()]
+    public class DateTests {
+        [TestMethod()]
+        public void DayOfYear() {
+            // arrange
+            IDate date = new TestUeben.Fakes.StubIDate() {
+                DayOfYearGet = () => { return 55; }
+            };
+
+            // act
+            int erg = date.DayOfYear;
+
+            // assert
+            Assert.AreEqual(erg, 55);
+        }
+
+    }
+}
